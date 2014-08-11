830dcd86f3b033f9eb94ffe04c153bc9f49aa01d
vikas chouhan <presentisgood@gmail.com>
2014-08-11 19:53:37 +0530

    Made enhancements for on the fly plots.

--- analysis.py
+++ analysis.py
@@ -75,13 +75,12 @@ class plots_class:
     def __init__(self, label=''):
         """Initialize a new figure object."""
         self.fig       = matplotlib.pyplot.figure(label)
-        self.datalist  = []
+        self.data      = {}
         self.plotted   = 0
         self.n_columns = 1
         self.n_plots   = 0
         self.n_rows    = 0
         self.h_ratios  = []
-        self.plot_type = []
 
     def new(self, n_plots, height_ratios=None, label=''):
         """Initialize the figure object. n_plots is the total number plots on this figure. \
@@ -124,54 +123,68 @@ class plots_class:
 
         # Draw previous plots again
         for i in range(0, (self.n_plots - 1)):
-            if self.plot_type[i] == self.PLOT_TYPE_PLOT:
-                self.plot(self.datalist[i][0], self.datalist[i][1], self.datalist[i][2])
-            elif self.plot_type[i] == self.PLOT_TYPE_BAR:
-                self.bar(self.datalist[i][0], self.datalist[i][1], self.datalist[i][2])
+            for dict_this in self.data[i]:
+                if dict_this["plot_type"] == self.PLOT_TYPE_PLOT:
+                    self.__draw(i, dict_this["x_list"], dict_this["y_list"], dict_this["label"], self.PLOT_TYPE_PLOT)
+                elif dict_this["plot_type"] == self.PLOT_TYPE_BAR:
+                    self.__draw(i, dict_this["x_list"], dict_this["y_list"], dict_this["label"], self.PLOT_TYPE_BAR)
+    
+    def __check_valid_frame(self, ratio, frame):
+        """Check if the frame is valid.If not, allocate a new frame."""
+        if frame == None:
+            if self.plotted >= self.n_plots:
+                self.__append_new(ratio)
+            return self.plotted
+        else:
+            assert(frame < self.n_plots)
+            return frame
 
-    def plot(self, x_list, y_list, label='', ratio=1):
+    def __append_data(self, frame, data):
+        """Append the plot data structure to the object's internal database."""
+        if not (frame in self.data):
+            self.data[frame] = []
+        self.data[frame].append(data)
+
+    def __plot(self, obj, x_list, y_list, label):
+        obj.plot(x_list, y_list, label)
+
+    def __bar(self, obj, x_list, y_list, label):
+        obj.bar(x_list, y_list, label)
+
+    def __draw(self, frame, x_list, y_list, label, plot_type):
+        """Internal plot."""
+        obj_this       = self.plot_obj[frame]
+        obj_this.grid()
+        obj_this.set_title(label)
+        if plot_type == self.PLOT_TYPE_PLOT:
+            self.__plot(obj_this, x_list, y_list, label)
+        elif plot_type == self.PLOT_TYPE_BAR:
+            self.__plot(obj_this, x_list, y_list, label)
+        self.fig.tight_layout()
+        self.__inc_plotted()
+
+    def plot(self, x_list, y_list, label='', ratio=1, frame=None):
         """Plot the actual data."""
-        # If the maximum limit exceeded, resize the number of plots
-        if self.plotted >= self.n_plots:
-            self.__append_new(ratio)
+        frame_new      = self.__check_valid_frame(ratio, frame)
+        self.__draw(frame_new, x_list, y_list, label, self.PLOT_TYPE_PLOT)
+        self.__append_data(frame_new,\
+                  {"x_list" : x_list, "y_list" : y_list, "label" : label, "plot_type" : self.PLOT_TYPE_PLOT})
+        
 
-        # Plot
-        obj_this       = self.plot_obj[self.plotted]
-        obj_this.grid()
-        obj_this.set_title(label)
-        obj_this.plot(x_list, y_list)
-        self.fig.tight_layout()
-       
-        # Update internal data structure 
-        self.datalist.append([x_list, y_list, label])
-        self.plot_type.append(self.PLOT_TYPE_PLOT)
-        self.__inc_plotted()
-
-    def bar(self, x_list, y_list, label='', ratio=1):
+    def bar(self, x_list, y_list, label='', ratio=1, frame=None):
         """Plot the actual data as bars."""
-        # If the maximum limit exceeded, resize the number of plots
-        if self.plotted >= self.n_plots:
-            self.__append_new(ratio)
+        frame_new      = self.__check_valid_frame(ratio, frame)
+        self.__draw(frame_new, x_list, y_list, label, self.PLOT_TYPE_BAR)
+        self.__append_data(frame_new,\
+                  {"x_list" : x_list, "y_list" : y_list, "label" : label, "plot_type" : self.PLOT_TYPE_PLOT})
 
-        # Plot
-        obj_this       = self.plot_obj[self.plotted]
-        obj_this.grid()
-        obj_this.set_title(label)
-        obj_this.bar(x_list, y_list)
-        self.fig.tight_layout()
-
-        # Update internal data structures
-        self.datalist.append([x_list, y_list, label])
-        self.plot_type.append(self.PLOT_TYPE_BAR)
-        self.__inc_plotted()
-
-    def plot_pandas_series(self, series, label='', ratio=1):
+    def plot_pandas_series(self, series, label='', ratio=1, frame=None):
         """Plot pandas.core.series.Series type data."""
         assert(type(series) == pandas.core.series.Series)
-        self.plot(series.index.tolist(), series.tolist(), label, ratio)
+        self.plot(series.index.tolist(), series.tolist(), label, ratio, frame)
 
-    def bar_pandas_series(self, series, label='', ratio=1):
+    def bar_pandas_series(self, series, label='', ratio=1, frame=None):
         """Plot pandas.core.series.Series type data as bars."""
         assert(type(series) == pandas.core.series.Series)
-        self.bar(series.index.tolist(), series.tolist(), label, ratio)
+        self.bar(series.index.tolist(), series.tolist(), label, ratio, frame)
 
