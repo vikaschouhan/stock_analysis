98bfedeee1d1977f65ba404d83dfe2efbb8ac9d1
vikas.chouhan <vi@vi.vi>
2014-08-15 16:22:47 +0545

    Adding copyright notice.

--- analysis.py
+++ analysis.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python
+# """"""""""""""""""""""""analysis.py"""""""""""""""""""""""""""""""""""
+# This file contains classes which facilitate technical analysis of stocks
+# for purpose of trading. Plotting facilities are based on matplotlib library.
+# Stock analysis is based on pandas library.
+#
+# Author : Vikas Chouhan (presentisgood@gmail.com)
+# Copyright 2014
+#
+# This library is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# It is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+# License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this file; see the file COPYING. If not, write to the
+# Free Software Foundation, 51 Franklin Street - Fifth Floor, Boston,
+# MA 02110-1301, USA.
 
 import datetime
 import pickle
