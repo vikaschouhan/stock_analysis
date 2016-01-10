877b305e1ee2bd3c2d7f023ff585547383167e17
vikas.chouhan <vi@vi.vi>
2016-01-10 18:09:24 +0545

    Added more strategies. Updated stock list file.

--- run_strategies.py
+++ run_strategies.py
@@ -10,6 +10,21 @@ import pandas as pd
 import pandas.io.data
 from   pandas import Series, DataFrame
 
+####################################################
+# convert to datetime format
+####################################################
+def convert_to_datetime_format(date_str):
+    regex_date_format1 = re.compile(r'(\d+):(\d+):(\d+)')
+    regex_date_format2 = re.compile(r'(\d+)-(\d+)-(\d+)')
+
+    if regex_date_format1.match(date_str):
+        res = regex_date_format1.match(date_str)
+    elif regex_date_format2.match(date_str):
+        res = regex_date_format2.match(date_str)
+    else:
+        raise Exception("date format wrong.")
+    return datetime.datetime(int(res.groups()[0]), int(res.groups()[1]), int(res.groups()[2]))
+
 #################################################
 # stock_data
 #################################################
@@ -36,14 +51,13 @@ class stock_data(object):
 
     def load_from_yahoo(self):
         """Load stock information from yahoo."""
-        self.stock_data      = pandas.io.data.get_data_yahoo(self.scripid, self.date_start, self.date_end)
-        self.__adj_close_s   = self.stock_data["Adj Close"]
-        self.__close_s       = self.stock_data["Close"]
-        self.__open_s        = self.stock_data["Open"]
-        self.__volume_s      = self.stock_data["Volume"]
-        self.__high_s        = self.stock_data["High"]
-        self.__low_s         = self.stock_data["Low"]
-        self.__date_s        = self.stock_data.index
+        self.data            = pandas.io.data.get_data_yahoo(self.scripid, self.date_start, self.date_end)
+        self.__adj_close_s   = self.data["Adj Close"]
+        self.__close_s       = self.data["Close"]
+        self.__open_s        = self.data["Open"]
+        self.__volume_s      = self.data["Volume"]
+        self.__high_s        = self.data["High"]
+        self.__low_s         = self.data["Low"]
 
     ## Getters
     def get_close(self):
@@ -72,10 +86,51 @@ class ma_strategy0(object):
         close_data = stk_data.get_adj_close()
         ewma_s     = pandas.ewma(close_data, self.ma_short)
         ewma_l     = pandas.ewma(close_data, self.ma_long)
-        if ewma_s[0] > ewma_l[0]:
+        if ewma_s[-1] > ewma_l[-1]:
             return True
         return False
 
+class ma_strategy_8ema_crossover(object):
+    def __init__(self, days_diff=2):
+        self.days_diff  = days_diff
+    # enddef
+
+    def __call__(self, stk_data):
+        close_data = stk_data.get_close()
+        open_data  = stk_data.get_open()
+        volume     = stk_data.get_volume()
+        ewma_c     = pandas.ewma(close_data, 8)
+        ewma_o     = pandas.ewma(open_data,  8)
+        ewma_vol   = pandas.rolling_mean(volume,  50)
+        # Check for 8ema crossover of open and close prices as well as
+        # a sudden spurt in volume
+        if ewma_c[-1] > ewma_o[-1] and \
+           ewma_c[-1-self.days_diff] <= ewma_o[-1-self.days_diff] and \
+           volume[-1] > 3 * ewma_vol[-1]:
+            return True
+        return False
+
+class ma_strategy2_8ema_crossover(object):
+    def __init__(self, days_diff=2):
+        self.days_diff  = days_diff
+    # enddef
+
+    def __call__(self, stk_data):
+        close_data = stk_data.get_close()
+        open_data  = stk_data.get_open()
+        volume     = stk_data.get_volume()
+        ewma_c     = pandas.ewma(close_data, 8)
+        ewma_o     = pandas.ewma(open_data,  8)
+        ewma_vol   = pandas.rolling_mean(volume,  50)
+        ewma_50    = pandas.ewma(close_data, 50)
+        # Check for 8ema crossover of open and close prices as well as
+        # a sudden spurt in volume
+        if ewma_c[-1] > ewma_o[-1] and \
+           ewma_c[-1-self.days_diff] <= ewma_o[-1-self.days_diff] and \
+           volume[-1] > 3 * ewma_vol[-1] and \
+           ewma_50[-1] > 0:
+            return True
+        return False
 
 ############################################
 # main
@@ -172,7 +227,7 @@ if __name__ == '__main__':
 
 
     # define strategy
-    strategy_this = ma_strategy0(8, 24)
+    strategy_this = ma_strategy2_8ema_crossover(4)
 
     for index in ticker_dict_n:
         try:
@@ -184,8 +239,3 @@ if __name__ == '__main__':
             #print "Couldn't load data for {}".format(index)
             pass
     # endfor
-
-
-    ## Dump global data structure to pickle file
-    #if pickle_file_passed == 0:
-    #    pickle.dump(glb_data_struct, open(args.dfile + ".pkl", "wb"))
